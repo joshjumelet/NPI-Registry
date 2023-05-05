27bed504b814d227092ca225ddee2ccb6a75e62d
Joshua Jumelet <joshuajumelet@gmail.com>
2023-05-05 13:53:10 -0700

    update ResultCard to show missing names

--- client/src/components/ResultCard.jsx
+++ client/src/components/ResultCard.jsx
@@ -4,7 +4,8 @@ const ResultCard = (props) => {
   return (
     <div className="result-card">
       <p>NPI: {props.number}</p>
-      <p>Name: {props.basic.first_name} {props.basic.last_name}</p>
+      <p>Name: {props.basic.first_name} {props.basic.last_name} {props.basic.authorized_official_first_name}{' '}
+        {props.basic.authorized_official_last_name}</p>
       <p>
         Address: {props.addresses[0].address_1},{' '}
         {props.addresses[0].city}, {props.addresses[0].state}{' '}
