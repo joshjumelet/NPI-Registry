bfe2f59439a92adeb1c54583c81ce55d459646b8
Joshua Jumelet <joshuajumelet@gmail.com>
2023-05-05 13:20:44 -0700

    build out App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,1 +1,19 @@
+import './App.css'
+import { Route, Routes } from 'react-router'
+import Home from './pages/Home'
+import ResultDetails from './pages/ResultDetails'
 
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/results/:number" element={<ResultDetails />} />
+        </Routes>
+      </main>
+    </div>
+  )
+}
+
+export default App
